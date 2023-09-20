f11f40734a94a7aeadbf8367a51b7aca9e42c228
OfficialKuutti <128027897+OfficialKuutti@users.noreply.github.com>
2023-09-20 13:30:26 +0300

    Commit 20.9.23_3
    Added: MainScene, MountainsScene, CaveScene, PauseMenu, LevelManager, Buttons, Font. SceneTransitions works for now between MainScene and MountainsScene.

---
+++ Assets/Scripts/LevelManagerScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public PauseScript pause;
+    public Animator fadeScreen;
+    public float transitionTime = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = GetComponent<PauseScript>();
+        //fadeScreen = GameObject.Find("FadeScreen").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Changelevel(int levelNumber)
+    {
+        pause.paused = false;
+        //fadeScreen.SetTrigger("ChangeLevel");
+        StartCoroutine(NewLevel(levelNumber));
+    }
+
+    public IEnumerator NewLevel(int levelNumber)
+    {
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelNumber);
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
---
+++ Assets/Scripts/PauseScript.cs
@@ -0,0 +1,59 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public bool paused = false;
+
+    public GameObject pauseMenu;
+    public GameObject player;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Button to toggle pause boolean
+        if (Input.GetButtonDown("Cancel"))
+        {
+            if (paused == true)
+            {
+                paused = false;
+            }
+
+            else if (paused == false)
+            {
+                paused = true;
+            }
+
+        }
+
+
+
+
+
+        // Pauses the game and opens the menu
+        if (paused == true)
+
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+        else if (paused == false)
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+
+    }
+}
