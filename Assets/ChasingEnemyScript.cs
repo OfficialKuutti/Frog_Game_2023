8030c64d8849bf64f8163be0cc4fc308ae58af14
Karvalakki <128028004+Karvalakki@users.noreply.github.com>
2023-09-25 10:49:48 +0300

    Revert "Commit21.9.2023_6"
    This reverts commit f0acf943440c16641bd0908bcd51dbf38c2999dd.

--- Assets/ChasingEnemyScript.cs
+++ Assets/ChasingEnemyScript.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-
+using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 public class ChasingEnemyScript : MonoBehaviour
 {
